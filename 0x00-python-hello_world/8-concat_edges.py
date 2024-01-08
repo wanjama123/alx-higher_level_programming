d0dfbd7a25434a30e588966311e7609df5b591ec
ian wanjama <ianwanjama78@gmail.com>
2024-01-07 22:23:31 -0800

    0x00. Python - Hello, World

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
+  
+  str = "Python is an interpreted, interactive, object-oriented programming\
+  language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str)
+    print(str)
