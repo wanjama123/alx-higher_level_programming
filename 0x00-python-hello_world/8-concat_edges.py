90a96fdfe8ace5c9b98faa358d52dd3369145fb6
ian wanjama <ianwanjama78@gmail.com>
2024-01-24 03:54:07 -0800

    0x00. Python - Hello, World

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-index = str.find("object-oriented programming")
-print(str[index:])
-
+str = "Python is an interpreted, interactive, object-oriented programming with python"
+print(str)
